fccd5272012738d1827c3dc395552f2faf2e6be9
DazHaze <dhayes1.wex@gmail.com>
2022-03-24 22:42:35 +0000

    add account signin/out links to website

--- PlantGram/settings.py
+++ PlantGram/settings.py
@@ -48,6 +48,11 @@ INSTALLED_APPS = [
     'plants',
 ]
 
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
