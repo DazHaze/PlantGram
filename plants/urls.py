fa2dd1bfc9222d76e1176293fa57b093806bafb9
DazHaze <dhayes1.wex@gmail.com>
2022-03-26 20:09:20 +0000

    fixed post detail url

--- plants/urls.py
+++ plants/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
+from .views import AddPostView 
 
 app_name = 'plants'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('?P<slug>[\w-]+/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('add_post/', views.AddPostView.as_view(), name='add_post'),
 ]
--- plants/views.py
+++ plants/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.views.generic import CreateView, DetailView
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic import (
@@ -16,12 +17,12 @@ class PostListView(ListView):
     template_name = 'index.html'
 
 
-class PostDetail(View):
+class PostDetail(DetailView):
 
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects
         post = get_object_or_404(queryset, slug=slug)
-        profile = request.user.get_profile()
+        profile = request.user.profile
         # comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
@@ -86,7 +87,7 @@ class PostLike(View):
         
         return HttpResponseRedirect(reverse('plants:post_detail', args=[slug]))
 
-class AddPostView(View):
+class AddPostView(CreateView):
     model = Post
     template_name = 'add_post.html'
     fields = '__all__'
